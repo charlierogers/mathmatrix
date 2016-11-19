f70a4e22b2f1fba01ca630f3ceb87f4c08c32210
rogerscc <rogerscc@umich.edu>
2016-11-19 16:53:26 -0500

    player 2 added in

--- game.js
+++ game.js
@@ -9,11 +9,13 @@ function preload() {
 
     game.load.image('atari', 'assets/atari130xe.png');
     game.load.image('sky', 'assets/sunset.png');
-
+    game.load.image('face', 'assets/happy.jpg')
 }
 
-var sprite;
+var player1;
 var cursors;
+var player2;
+var wasd;
 
 function create() {
 
@@ -25,43 +27,102 @@ function create() {
     //  Make things a bit more bouncey
     game.physics.p2.defaultRestitution = 0.8;
 
+
     //  Add a sprite
-    sprite = game.add.sprite(200, 200, 'atari');
+    player1= game.add.sprite(200, 200, 'atari');
+    player2= game.add.sprite(300,300, 'face');
+    player2.scale.setTo(.1,.1);
 
     //  Enable if for physics. This creates a default rectangular body.
-    game.physics.p2.enable(sprite);
+
+    game.physics.p2.enable(player1);
+    game.physics.p2.enable(player2);
 
     //  Modify a few body properties
-    sprite.body.setZeroDamping();
-    sprite.body.fixedRotation = true;
+    player1.body.setZeroDamping();
+    player1.body.fixedRotation = true;
 
-    text = game.add.text(20, 20, 'move with arrow keys', { fill: '#ffffff' });
+    player2.body.setZeroDamping();
+    player2.body.fixedRotation = true;
+
+
+    text = game.add.text(20, 20, 'move with ', { fill: '#ffffff' });
 
     cursors = game.input.keyboard.createCursorKeys();
+
+    //
+    // wasd = {
+    //     up: game.input.keyboard.addKey(Phaser.Keyboard.W),
+    //     down: game.input.keyboard.addKey(Phaser.Keyboard.S),
+    //     left: game.input.keyboard.addKey(Phaser.Keyboard.A),
+    //     right: game.input.keyboard.addKey(Phaser.Keyboard.D),
+    // }
+        game.input.keyboard.addKey(Phaser.Keyboard.W);
+        game.input.keyboard.addKey(Phaser.Keyboard.S);
+        game.input.keyboard.addKey(Phaser.Keyboard.A);
+        game.input.keyboard.addKey(Phaser.Keyboard.D);
 
 }
 
 function update() {
 
-    sprite.body.setZeroVelocity();
+    // console.log ("up: " + wasd.up.isDown);
+    // console.log("down:" + wasd.down.isDown);
+    // console.log("right:" + wasd.right.isDown);
+    // console.log("left" + wasd.left.isDown);
+
+    player2.body.setZeroVelocity();
+
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.S))
+    {
+
+        player2.body.moveDown(400)
+    }
+    if (game.input.keyboard.isDown(Phaser.Keyboard.D))
+    {
+
+        player2.body.moveRight(400);
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+    {
+
+        player2.body.moveLeft(400);
+    }
+    if (game.input.keyboard.isDown(Phaser.Keyboard.W))
+    {
+
+        player2.body.moveUp(400);
+    }
+
+
+
+
+
+
+
+
+
+    player1.body.setZeroVelocity();
 
     if (cursors.left.isDown)
     {
-        sprite.body.moveLeft(400);
+        player1.body.moveLeft(400);
     }
     else if (cursors.right.isDown)
     {
-        sprite.body.moveRight(400);
+        player1.body.moveRight(400);
     }
 
     if (cursors.up.isDown)
     {
-        sprite.body.moveUp(400);
+        player1.body.moveUp(400);
     }
     else if (cursors.down.isDown)
     {
-        console.log("moving down");
-        sprite.body.moveDown(400);
+
+        player1.body.moveDown(400);
     }
 
 }
